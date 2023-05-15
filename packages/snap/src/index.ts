e00504c107bd9841bd9795fec92da5893a0481e4
hmalik88 <hassan.malik@consensys.net>
2023-05-15 03:56:48 -0400

    fix

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,60 +1,26 @@
-import { OnRpcRequestHandler } from '@metamask/snaps-types';
-import { panel, text } from '@metamask/snaps-ui';
+const lensLookup = async function (name: string) {
+  const query = `query Profile { profile(request: { handle: "${name}" }) { ownedBy } }`;
+  const result = await fetch(
+    `https://api.lens.dev/?query=${encodeURIComponent(query)}&operationName=Profile`,
+  );
+  return result.json();
+};
 
-const lensLookup = async function(name:String) { 
-  const query = `query Profile { profile(request: { handle: "${name}" }) { ownedBy } }`; 
-  const result = await fetch('https://api.lens.dev/?query='+encodeURIComponent(query)+'&operationName=Profile'); 
-  return result.json(); 
-}; 
-
-const query = 'query Profile { profile(request: { handle: "m0nt0y4.lens" }) { ownedBy } }'; 
+// const query =
+//   'query Profile { profile(request: { handle: "m0nt0y4.lens" }) { ownedBy } }';
 
 /**
  * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
  *
  * @param args - The request handler args as object.
- * @param args.origin - The origin of the request, e.g., the website that
- * invoked the snap.
- * @param args.request - A validated JSON-RPC request object.
- * @returns The result of `snap_dialog`.
- * @throws If the request method is not valid for this snap.
+ * @param args.content - The domain to be resolved.
+ * @returns The result of the lens lookup.
  */
-export const onRpcRequest: OnRpcRequestHandler = async ({ origin, request }) => {
-  switch (request.method) {
-    case 'hello':
-      const query = await snap.request({ 
-        method: 'snap_dialog', 
-        params: { 
-          type: 'prompt', 
-          content: panel([
-            text('**Lookup a Lens ID**'), 
-            text('Enter the name to lookup:'), 
-          ]), 
-        }
-      }); 
-      const response = []; 
-      if(typeof query == 'string') { 
-        const result = await lensLookup(query.trim()); 
-        const profile = result.data.profile; 
-        if(profile && profile.hasOwnProperty("ownedBy")) { 
-          response.push(text(`**Address found for ${query}!**`)); 
-          response.push(text(profile.ownedBy)); 
-        }
-        else { 
-          response.push(text(`No address was found for ${query}.`)); 
-        }
-      }
-      else { 
-        response.push(text('Please enter a name to lookup!')); 
-      }
-      return snap.request({
-        method: 'snap_dialog',
-        params: {
-          type: 'alert',
-          content: panel(response),
-        },
-      });
-    default:
-      throw new Error('Method not found.');
+export const onNameLookup = async ({ content }: any) => {
+  const result = await lensLookup(content);
+  const { profile } = result.data;
+  if (profile?.ownedBy) {
+    return { resolvedAccount: profile.ownedBy };
   }
+  return { resolvedAccount: null };
 };
